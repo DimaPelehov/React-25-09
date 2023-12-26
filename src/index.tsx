4e360f97c19c0469a71bd602f408f741bb5faaed
Dmytro <pelexov.mitya@gmail.com>
2023-12-26 19:56:51 +0200

    write code with jsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+// const el = React.createElement('h1', { id: 'title' }, 'Hello React')
+// це застарілий спосіб, без jsx(розширений js)
+
+const a = 10
+const el = <h1 id="title">Hello React, {a}</h1>
+// цей спосіб прописано з jsx
+root.render(<React.StrictMode>{el}</React.StrictMode>)
