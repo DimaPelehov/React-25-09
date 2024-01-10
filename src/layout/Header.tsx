71d6514759d9e48d8aa9d40e38c68faeaeaa8ac7
Dmytro <pelexov.mitya@gmail.com>
2024-01-10 18:43:21 +0200

    add Menu and CartHeader

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,12 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Button from '@mui/material/Button'
 // import IconButton from '@mui/material/IconButton'
 // import MenuIcon from '@mui/icons-material/Menu'
 // це треба видалити,оскільки замість цього ми вставили логотип
 import Container from '@mui/material/Container'
 import Logo from 'components/Logo/Logo'
+import Menu from 'components/Menu/Menu'
+import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
 
@@ -20,7 +21,8 @@ const Header = (props: Props) => {
             <Container maxWidth="lg">
                 <Toolbar>
                     <Logo />
-                    <Button color="inherit">Login</Button>
+                    <Menu />
+                    <CartHeader />
                 </Toolbar>
             </Container>
         </AppBar>
