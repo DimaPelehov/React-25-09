57b90336e48e30b90ba535a94da3d5849c7732b6
Dmytro <pelexov.mitya@gmail.com>
2024-01-11 15:21:43 +0200

    створили ProductListItem, додали css, вивели в ProductList->Home->Main

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,5 +1,14 @@
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
+
 type Props = {}
+
 const Main = (props: Props) => {
-    return <div>Main</div>
+    return (
+        <Container maxWidth="lg" sx={{ padding: '50px 0px' }}>
+            <Home />
+        </Container>
+    )
 }
+
 export default Main
