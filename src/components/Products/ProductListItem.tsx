ef0c2e5202856e2fd02ea39bf12829eb2d5fde8e
Dmytro <pelexov.mitya@gmail.com>
2024-01-18 19:56:14 +0200

    замінили функцію ProductListItem на class ProductListItem

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, CardContent, TextField } from '@mui/material'
 import './ProductListItem.css'
+import { Component } from 'react'
 
 type ProductListItemType = {
     title: string
@@ -10,38 +11,73 @@ type ProductListItemType = {
     image: string
 }
 
-const ProductListItem = ({
-    title,
-    description,
-    type = 'phone',
-    // якщо в ProductList.tsx не буде зазначено type, то він пропишеться за замовчуванням
-    capacity,
-    price,
-    image,
-}: ProductListItemType) => {
-    return (
-        <Card className="product-list-item" variant="outlined">
-            <CardContent>
-                <div className="product-img">
-                    <img src={image} alt={title} />
-                </div>
-                <h2 className="product-title">{title}</h2>
-                <p className="product-description">{description}</p>
-                <div className="product-features">Type:{type}</div>
-                <div className="product-features">Capacity:{capacity} Gb</div>
-                <div className="product-price">$ {price}</div>
-                <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value={1}></TextField>
-                    <Button variant="outlined">+</Button>
-                </div>
+// замінюємо функцію ProductListItem на class ProductListItem
+class ProductListItem extends Component<ProductListItemType> {
+    render() {
+        return (
+            <Card className="product-list-item" variant="outlined">
+                <CardContent>
+                    <div className="product-img">
+                        <img src={this.props.image} alt={this.props.title} />
+                    </div>
+                    <h2 className="product-title">{this.props.title}</h2>
+                    <p className="product-description">
+                        {this.props.description}
+                    </p>
+                    <div className="product-features">
+                        Type:{this.props.type}
+                    </div>
+                    <div className="product-features">
+                        Capacity:{this.props.capacity} Gb
+                    </div>
+                    <div className="product-price">$ {this.props.price}</div>
+                    <div className="product-quantity">
+                        <Button variant="outlined">-</Button>
+                        <TextField size="small" value={1}></TextField>
+                        <Button variant="outlined">+</Button>
+                    </div>
 
-                <div className="btns-wrap">
-                    <Button variant="outlined">Add to cart</Button>
-                </div>
-            </CardContent>
-        </Card>
-    )
+                    <div className="btns-wrap">
+                        <Button variant="outlined">Add to cart</Button>
+                    </div>
+                </CardContent>
+            </Card>
+        )
+    }
 }
+
+// const ProductListItem = ({
+//     title,
+//     description,
+//     type = 'phone',
+//     // якщо в ProductList.tsx не буде зазначено type, то він пропишеться за замовчуванням
+//     capacity,
+//     price,
+//     image,
+// }: ProductListItemType) => {
+//     return (
+//         <Card className="product-list-item" variant="outlined">
+//             <CardContent>
+//                 <div className="product-img">
+//                     <img src={image} alt={title} />
+//                 </div>
+//                 <h2 className="product-title">{title}</h2>
+//                 <p className="product-description">{description}</p>
+//                 <div className="product-features">Type:{type}</div>
+//                 <div className="product-features">Capacity:{capacity} Gb</div>
+//                 <div className="product-price">$ {price}</div>
+//                 <div className="product-quantity">
+//                     <Button variant="outlined">-</Button>
+//                     <TextField size="small" value={1}></TextField>
+//                     <Button variant="outlined">+</Button>
+//                 </div>
+
+//                 <div className="btns-wrap">
+//                     <Button variant="outlined">Add to cart</Button>
+//                 </div>
+//             </CardContent>
+//         </Card>
+//     )
+// }
 
 export default ProductListItem
